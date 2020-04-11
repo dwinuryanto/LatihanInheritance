f8e4a7de020f823f4e8cff40748ef900ea7b6578
dwinuryanto <yantodwinur29@gmail.com>
2020-04-11 15:35:33 +0700

    -

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Dwi", 20);
+            person.GetNameAndAge();
+            Console.WriteLine();
+            Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
+            teacher.GetNameAndAge();
+            Console.WriteLine();
+            Student student = new Student("DWI", 20, "19.11.2624", "dwi.0407@students.amikom.ac.id");
+            student.GetNameAndAge();
+            Console.ReadKey();
+
+        }
+    }
+}
